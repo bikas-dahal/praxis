773901ea58d9c7759c387c398f0703aacad0cfe4
Bikas Dahal <bikkydahal@gmail.com>
2024-12-08 16:58:48 +0545

    trying netlify

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   pageExtensions: ['mdx', 'ts', 'tsx'],
+  output: 'standalone',
   typescript: {
     ignoreBuildErrors: true
   },
